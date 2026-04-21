5cd606796de75d052fdf3d3bea901bcc4bfc353d
CLLR <3228847917@qq.com>
2026-04-21 21:37:34 +0800

    方案二任务

---
+++ Task/ARM2.c
@@ -0,0 +1,54 @@
+#include "ARM2.h"
+#include "Task_Init.h"
+#include "app_motor_usart.h"
+#include "bsp_motor_usart.h"
+#include "PID_old.h"
+
+motor_PID Motor1_PID;
+motor_PID Motor2_PID;
+motor_PID Motor3_PID;
+
+Param motor1;
+Param motor2;
+Param motor3;
+
+extern int Encoder_Offset[4];
+extern int Encoder_Now[4];
+extern float g_Speed[4];
+extern uint8_t g_recv_flag;
+
+TaskHandle_t ARM2_Handle;
+void ARM2(void *pvParameters)
+{
+ TickType_t Last_wake_time = xTaskGetTickCount();
+	
+	Contrl_Pwm(0,0,0,NULL);// 配置三路电机PWM
+	
+	send_upload_data(true, true, true);
+	
+	// 配置全部电机(广播)
+	send_motor_type(MOTOR_520);// 配置电机类型
+	vTaskDelay(100);
+	send_pulse_phase(30);//配置减速比 查电机手册得出
+	vTaskDelay(100);
+	send_pulse_line(11);//配置磁环线 查电机手册得出
+	vTaskDelay(100);
+	send_wheel_diameter(67.00);//配置轮子直径,测量得出
+	vTaskDelay(100);
+	send_motor_deadzone(1900);//配置电机死区,实验得出
+	vTaskDelay(100);
+	
+	for(;;)
+	{
+		// 电机 1 (底座)
+		PID_Control2(motor1.current_encoder, Encoder_Offset[0], &Motor1_PID.pid);
+		// 电机 2 
+		PID_Control2(motor2.current_encoder, Encoder_Offset[1], &Motor2_PID.pid);
+		// 电机 3 
+		PID_Control2(motor3.current_encoder, Encoder_Offset[2], &Motor3_PID.pid);
+		
+		Contrl_Speed((int16_t)Motor1_PID.pid.pid_out, (int16_t)Motor2_PID.pid.pid_out, (int16_t)Motor3_PID.pid.pid_out, NULL);
+		
+ vTaskDelayUntil(&Last_wake_time, pdMS_TO_TICKS(2));
+	}
+}
---
+++ Task/ARM2.h
@@ -0,0 +1,28 @@
+#ifndef _ARM2_H_
+#define _ARM2_H_
+
+#include "Task_Init.h"
+
+//typedef struct
+//{
+//	PID2 vel_pid;
+//	PID2 pos_pid;
+//}TIM_PID;
+
+//typedef struct
+//{
+//	float Exp_pos;
+//	float Exp_vel;
+//}TIM_Exp_Prama;
+
+typedef struct
+{
+PID2 pid;
+}motor_PID;
+
+typedef struct
+{
+	int16_t current_encoder;
+}Param;
+
+#endif
