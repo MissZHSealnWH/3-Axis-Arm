a0daf41b9272abe5d9ac9b74990d61554b1dceeb
CLLR <3228847917@qq.com>
2026-05-01 14:41:12 +0800

    增添总计数（解算成功）

--- MyTask/ARM2.c
+++ MyTask/ARM2.c
@@ -7,7 +7,7 @@
 #include "micro_kdl.h"
 #include "uplink_drv.h"
 
-static Mikdl_Robot robot;             // 机械臂结构体
+static Mikdl_Robot RobotArm;             // 机械臂结构体
 static Mikdl_Vector3 q_curr;          // 当前关节角度
 static Mikdl_Vector3 p_curr;          // 当前末端位置
 
@@ -19,12 +19,12 @@ static float total_dist;
 static uint32_t traj_start_tick;
 static uint8_t traj_active = 0;
 
-
-
+// PID
 motor_PID Motor1_PID;
 motor_PID Motor2_PID;
 motor_PID Motor3_PID;
 
+// 期望参数 
 Param motor1;
 Param motor2;
 Param motor3;
@@ -71,29 +71,29 @@ void ARM2(void *pvParameters)
 	}
 }
 
-void Robot_Init(void)
+void RobotArm_Init(void)
 {
-    mikdl_robot_default(&robot);
-    robot.l1 = 0.2f;
-    robot.l2 = 0.15f;
-    robot.m1 = 0.5f;
-    robot.m2 = 0.3f;
-    robot.c1 = 0.1f;
-    robot.c2 = 0.075f;
+    mikdl_robot_default(&RobotArm);
+    RobotArm.l1 = 0.2f;
+    RobotArm.l2 = 0.15f;
+    RobotArm.m1 = 0.5f;
+    RobotArm.m2 = 0.3f;
+    RobotArm.c1 = 0.1f;
+    RobotArm.c2 = 0.075f;
 
     // 初始关节角度 (0, 30, 30)
     q_curr.x = 0.0f;
     q_curr.y = 0.5236f;
     q_curr.z = 0.5236f;
 
-    mikdl_forward_kinematics(&robot, &q_curr, &p_curr);
+    mikdl_forward_kinematics(&RobotArm, &q_curr, &p_curr);
 }
 
 UplinkCommand cmd;
 TaskHandle_t Analysis_Handle;
 void Analysis(void *pvParameters)
 {
-	Robot_Init();
+	RobotArm_Init();
 	
  TickType_t Last_wake_time = xTaskGetTickCount();
 	for(;;)
@@ -157,37 +157,35 @@ void Analysis(void *pvParameters)
 		} 
 			else 
 			{
-				// 没有轨迹就保持当前位置，速度/加速度为0
-					p_ref = p_curr;
-					v_ref.x = 0; v_ref.y = 0; v_ref.z = 0;
-					a_ref.x = 0; a_ref.y = 0; a_ref.z = 0;
+			// 没有轨迹就保持当前位置，速度/加速度为0
+				p_ref = p_curr;
+				v_ref.x = 0; v_ref.y = 0; v_ref.z = 0;
+				a_ref.x = 0; a_ref.y = 0; a_ref.z = 0;
 			}
-
-				// 调用micro_kdl解算
-				int ret = mikdl_cart_to_joint_control
-						(&robot,
-						 &p_ref, 
-						 &v_ref,
-						 &a_ref,
-						 &f_ext,
-						 &q_curr,
-						 &q_out, 
-						 &dq_out,
-						 &tau_out);
+			// 调用micro_kdl解算
+			int ret = mikdl_cart_to_joint_control
+					(&RobotArm,
+					 &p_ref, 
+					 &v_ref,
+					 &a_ref,
+					 &f_ext,
+					 &q_curr,
+					 &q_out, 
+					 &dq_out,
+					 &tau_out);
 				
 				if (ret == MIKDL_SUCCESS) 
 				{
-						q_curr = q_out;
-						mikdl_forward_kinematics(&robot, &q_curr, &p_curr);
+					q_curr = q_out;
+					mikdl_forward_kinematics(&RobotArm, &q_curr, &p_curr);
+					ALL_num++;
 				} 
         else 
 				{
-					// 解算失败就保持上一周期关节角度不动
+					// 解算失败保持上一周期关节角度
 					q_out = q_curr;
         }
-
-        // 将 q_out 转换成电机期望位置（单位要对应编码器单位！）
-
+        // 将 q_out 转换成电机期望位置
         motor1.Exp_encoder = (int32_t)(q_out.x * RAD2ENC_FACTOR_JOINT1 + 0.5f);
         motor2.Exp_encoder = (int32_t)(q_out.y * RAD2ENC_FACTOR_JOINT2 + 0.5f);
         motor3.Exp_encoder = (int32_t)(q_out.z * RAD2ENC_FACTOR_JOINT3 + 0.5f);
@@ -204,8 +202,8 @@ void Analysis(void *pvParameters)
 				(int16_t)Motor2_PID.pid.pid_out, 
 				(int16_t)Motor3_PID.pid.pid_out, 
 				 NULL);
-		
-
+				
+				
  vTaskDelayUntil(&Last_wake_time, pdMS_TO_TICKS(10));
 	}
 }
