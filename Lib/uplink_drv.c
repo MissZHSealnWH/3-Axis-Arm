9a8fb99a955af70c8fdbba3dbe320df486ff18bb
CLLR <3228847917@qq.com>
2026-05-01 14:47:11 +0800

    通信增添三个扩展待定义的字节

--- Lib/uplink_drv.c
+++ Lib/uplink_drv.c
@@ -2,36 +2,45 @@
 #include "stm32h7xx_hal.h"
 #include <string.h>
 
-extern UART_HandleTypeDef huart5;
-extern DMA_HandleTypeDef hdma_uart5_rx;
+extern UART_HandleTypeDef huart1;
+extern DMA_HandleTypeDef hdma_usart1_rx;
 
 static uint8_t dma_rx_buf[UPLINK_FRAME_LEN];
 static UplinkCommand latest_cmd;
 static volatile uint8_t new_cmd_flag = 0;
 
-void Uplink_Init(void) {
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart5, dma_rx_buf, UPLINK_FRAME_LEN);
-    __HAL_DMA_DISABLE_IT(&hdma_uart5_rx, DMA_IT_HT);
+void Uplink_Init(void) 
+{
+	HAL_UARTEx_ReceiveToIdle_DMA(&huart1, dma_rx_buf, UPLINK_FRAME_LEN);
+	__HAL_DMA_DISABLE_IT(&hdma_usart1_rx, DMA_IT_HT);
 }
 
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
-    if (huart->Instance == UART5 && Size == UPLINK_FRAME_LEN) {
-        if (dma_rx_buf[0] == FRAME_START && dma_rx_buf[4] == FRAME_LAST) {
-            latest_cmd.x = (int8_t)dma_rx_buf[1];
-            latest_cmd.y = (int8_t)dma_rx_buf[2];
-            latest_cmd.z = (int8_t)dma_rx_buf[3];
-            new_cmd_flag = 1;
-        }
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) 
+{
+    if (huart->Instance == UART5 && Size == UPLINK_FRAME_LEN) 
+		{
+			if (dma_rx_buf[0] == FRAME_START && dma_rx_buf[7] == FRAME_LAST) 
+			{
+				latest_cmd.x =     (int8_t)dma_rx_buf[1];
+				latest_cmd.y =     (int8_t)dma_rx_buf[2];
+				latest_cmd.z =     (int8_t)dma_rx_buf[3];
+				latest_cmd.one =   (int8_t)dma_rx_buf[4];
+				latest_cmd.two =   (int8_t)dma_rx_buf[5];
+				latest_cmd.three = (int8_t)dma_rx_buf[6];
+				new_cmd_flag = 1;
+			}
     }
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart5, dma_rx_buf, UPLINK_FRAME_LEN);
-    __HAL_DMA_DISABLE_IT(&hdma_uart5_rx, DMA_IT_HT);
+	HAL_UARTEx_ReceiveToIdle_DMA(&huart1, dma_rx_buf, UPLINK_FRAME_LEN);
+	__HAL_DMA_DISABLE_IT(&hdma_usart1_rx, DMA_IT_HT);
 }
 
-int Uplink_GetCommand(UplinkCommand *cmd) {
-    if (new_cmd_flag) {
-        new_cmd_flag = 0;
-        *cmd = latest_cmd;
-        return 1;
-    }
-    return 0;
+int Uplink_GetCommand(UplinkCommand *cmd) 
+{
+	if (new_cmd_flag) 
+	{
+		new_cmd_flag = 0;
+		*cmd = latest_cmd;
+		return 1;
+	}
+	return 0;
 }
--- Lib/uplink_drv.h
+++ Lib/uplink_drv.h
@@ -3,15 +3,18 @@
 
 #include <stdint.h>
 
-#define UPLINK_FRAME_LEN 5   // 2(包头包尾) + 3*1(int8_t)
+#define UPLINK_FRAME_LEN 8   // 2(包头包尾) + 3*1(int8_t) + 3*1
 #define FRAME_START 0xAA     // 包头
 #define FRAME_LAST 0x55      // 包尾
 
-// 三个轴的命令数据 (char)
+// 三个轴的命令数据 (char) + 其他待扩展
 typedef struct {
-    int8_t x;
-    int8_t y;
-    int8_t z;
+	int8_t x;
+	int8_t y;
+	int8_t z;
+	int8_t one;
+	int8_t two;
+	int8_t three;
 } UplinkCommand;
 
 void Uplink_Init(void);
