2bfee6652fb048030786586a9cb14c4891e59ff5
CLLR <3228847917@qq.com>
2026-05-02 09:27:14 +0800

    矫正核心文件

--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -58,18 +58,10 @@ void DMA1_Stream2_IRQHandler(void);
 void DMA1_Stream3_IRQHandler(void);
 void DMA1_Stream4_IRQHandler(void);
 void DMA1_Stream5_IRQHandler(void);
-void DMA1_Stream6_IRQHandler(void);
-void FDCAN1_IT0_IRQHandler(void);
 void TIM1_UP_IRQHandler(void);
-void USART2_IRQHandler(void);
-void DMA1_Stream7_IRQHandler(void);
-void UART4_IRQHandler(void);
-void UART5_IRQHandler(void);
-void DMA2_Stream0_IRQHandler(void);
-void DMA2_Stream1_IRQHandler(void);
-void FDCAN_CAL_IRQHandler(void);
-void USART6_IRQHandler(void);
+void USART1_IRQHandler(void);
 void UART7_IRQHandler(void);
+void USART10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
