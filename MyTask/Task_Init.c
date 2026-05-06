1c3ca250db29d5d68cb3ecfe8b2184c76da2eaec
CLLR <3228847917@qq.com>
2026-05-06 22:09:10 +0800

    修改电机驱动初始化

--- MyTask/Task_Init.c
+++ MyTask/Task_Init.c
@@ -7,12 +7,12 @@
 extern volatile uint8_t g_recv_flag; 
 extern uint8_t g_recv_buff[RXBUFF_LEN];
 extern uint8_t g_recv_buff_deal[RXBUFF_LEN];
-uint8_t uart7_rx_byte;
 
 void Task_Init(){
-	
+
+
   // 与电机驱动模块通信
-	HAL_UART_Receive_IT(&huart7, &uart7_rx_byte, 1);
+	Motor_Usart_Init();
 	
 	// 与上位机视觉通信
 	Uplink_Init();
@@ -38,26 +38,4 @@ void Task_Init(){
 }
 
 
-
-
-// 电机反馈
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    if(huart->Instance == UART7)
-    {
-			Deal_Control_Rxtemp(uart7_rx_byte);
-
-			// 重新打开接收
-			HAL_UART_Receive_IT(&huart7, &uart7_rx_byte, 1);
-    }
-}
-
-void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
-{
-    if(huart->Instance == UART7) 
-		{
-			HAL_UART_Receive_IT(&huart7, &uart7_rx_byte, 1);  // 重启动接收
-    }
-}
-/**/
 
