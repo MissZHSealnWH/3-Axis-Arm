7bc512eac851bd63bb59b946d5775a4d33ccf489
CLLR <3228847917@qq.com>
2026-06-09 10:08:30 +0800

    更新通信逻辑（最终）

--- Lib/uplink_drv.h
+++ Lib/uplink_drv.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-#define UPLINK_FRAME_LEN 14   // 2(包头包尾) + 3*1(int16_t) + 3*1
-#define FRAME_START 0xAA     // 包头
-#define FRAME_LAST 0x55      // 包尾
+#define UPLINK_FRAME_LEN 8   // 2(包头包尾) + 3*1(int16_t) + 3*1
+#define FRAME_START 0x41     // 包头
+#define FRAME_LAST 0x42      // 包尾
 
 // 三个轴的命令数据 (int16) + 其他待扩展
-typedef struct {
+volatile typedef struct {
 	int8_t x; // 上下
 	int8_t y; // 左右
 	int8_t z; // 摄像头前方 沿小臂向前（平行小臂）
